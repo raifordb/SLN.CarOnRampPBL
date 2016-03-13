e7a34c12e5d4428713058d38e18f73ed35527120
raifordb <raifordb@live.com>
2016-03-13 09:42:27 -0400

    First

---
+++ CarOnRampPBL/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using CarOnRampPBL.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace CarOnRampPBL.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
